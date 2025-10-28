097c11bc5298ebdc3c27b15dbd7ea2b142f7eaa9
Souleymane DIALLO <sdley2020@gmail.com>
2025-10-28 17:16:51 +0000

    chore: updated Main program

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,6 @@
-public class Main {
+void main() {
 
-    public static void main(String[] args) {
-
-        System.out.println("I like pizza!");
-        System.out.println("It is very good.");
-        System.out.println("Buy me pizza!");
-    }
+    System.out.println("I like pizza!");
+    System.out.println("It is very good.");
+    System.out.println("Buy me pizza!");
 }
