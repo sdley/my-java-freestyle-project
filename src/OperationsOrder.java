fe95c5580109724f6b7f3745c61b370b023160ad
Souleymane DIALLO <sdley2020@gmail.com>
2025-10-29 12:49:12 +0000

    feat: add Operations Order
    - Showcasing operations order

---
+++ src/OperationsOrder.java
@@ -0,0 +1,12 @@
+void main() {
+
+    // Operations Order [PEMDAS / BODMAS]
+    // PEMDAS: Parentheses, Exponents, Multiplication and Division (from left to right),
+    //                                      Addition and Subtraction (from left to right)
+    // BODMAS: Brackets, Orders (i.e. powers and square roots, etc.), Division and Multiplication (from left to right),
+    //                                                                    Addition and Subtraction (from left to right)
+
+    double result = 3 + 2 * (4 - 1) / 3.0;
+
+    System.out.println("Result: " + result); // Result: 5.0
+}
